61cadf0ca44e7303fae5b9693671af3b5a76f8eb
4D 2016-17 <4D 2016-17@PC07>
2017-05-11 12:46:09 +0200

    Faccia nella faccia

--- src/questionario/FXMLControllerWebCam.java
+++ src/questionario/FXMLControllerWebCam.java
@@ -73,8 +73,14 @@ public class FXMLControllerWebCam implements Initializable {
     private ImageView imvCam2;
 
     @FXML
-    void doAdd(ActionEvent event) {
+    void doAdd(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("SceltaSiNo.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
 
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(tableViewScene);
+        window.show();
     }
 
     @FXML
@@ -141,69 +147,49 @@ public class FXMLControllerWebCam implements Initializable {
     @FXML
     void doDetection(ActionEvent event) throws IOException {
         int i = 1;
-
-        faceDetector = new CascadeClassifier("C:\\Users\\4D_2016-17\\Desktop\\JavaFXOpenCvTask\\src\\javafxopencvtask\\haarcascade_frontalface_alt.xml");
-
-        faceDetector.detectMultiScale(frame, faceDetections);
-        int x = faceDetections.width();
-        int y = faceDetections.height();
-      /*  Mat matAdd = Imgcodecs.imread("C:\\Users\\4D_2016-17\\Desktop\\JavaFXOpenCvTask\\src\\javafxopencvtask\\Pinocchio.jpg", Imgcodecs.CV_LOAD_IMAGE_ANYCOLOR);
-        Mat newMat = new Mat();
-       // Size s = new Size(x, y);
-        Size s = new Size(x, y);
-        Imgproc.resize(matAdd, newMat, s);
-        //Rect[] rect =faceDetections.toArray();
-        Imgproc.line(frame, new Point(20, 20), new Point(400, 400), new Scalar(0, 0, 255));*/
-        /* roi= new Rect(new Point(rect1.x, rect1.y), s);
-             Mat destinationROI = frame.submat( roi );
-             newMat.copyTo( destinationROI , newMat); 
-            System.out.println(i++);*/
+        Mat matAdd = Imgcodecs.imread("C:\\Users\\4D_2016-17\\Desktop\\prova.jpg", Imgcodecs.CV_LOAD_IMAGE_ANYCOLOR);
+        Mat shrek = Imgcodecs.imread("C:\\Users\\4D_2016-17\\Desktop\\index2.jpg", Imgcodecs.CV_LOAD_IMAGE_ANYCOLOR);
+        faceDetector = new CascadeClassifier("C:\\Users\\4D_2016-17\\Desktop\\SIMONECOGLIONE\\src\\javafxopencvtask\\haarcascade_frontalface_alt.xml");
        
-        //Rect[] rect =faceDetections.toArray();
-        
+         Rect shrekroi= new Rect(150,30,60,75);
+         Mat faccia=new Mat();
+         Mat destinationROI=matAdd.submat(shrekroi);
+         Size s = new Size(60, 75);
+         faceDetector.detectMultiScale(frame, faceDetections);
+       
      
-       
         Imgproc.line(frame, new Point(20, 20), new Point(400, 400), new Scalar(0, 0, 255));
         Rect rect2=new Rect();
-        //Point a=new Point(rect[0].x,rect[0].y);
+        Mat newMat= new Mat();
+       
         for ( Rect rect1 : faceDetections.toArray()) {
                
-        //Rect[] rect =faceDetections.toArray();
+           
+            //Imgproc.circle(frame, new Point(rect1.x+rect1.width/2,rect1.y+rect1.height/2),rect1.width/2, new Scalar(0, 0, 255), 5);
+            //Imgproc.rectangle(frame, new Point(rect1.x, rect1.y), new Point(rect1.x + rect1.width, rect1.y + rect1.height+((rect1.height*15)/100)), new Scalar(0, 0, 255), 5);
+            Rect roi= new Rect(rect1.x,rect1.y,rect1.width,rect1.height+((rect1.height*15)/100));
+            Mat crop=new Mat();
+            crop=frame.submat(roi);
+            Imgproc.resize(crop, newMat, s);
+           
             
-            Imgproc.rectangle(frame, new Point(rect1.x, rect1.y), new Point(rect1.x + rect1.width, rect1.y + rect1.height), new Scalar(0, 0, 255), 5);
-            rect2=rect1;
-            //newMat.copyTo(frame);
-            //
+           newMat.copyTo( destinationROI); 
+           shrek.copyTo( matAdd, shrek ); 
+          
         }
-           //Rect roi= new Rect(rect2.x, rect2.y,x,y);
-            Mat matAdd = Imgcodecs.imread("C:\\Users\\4D_2016-17\\Desktop\\JavaFXOpenCvTask1\\src\\javafxopencvtask\\LAVORO_SCUOLA.jpg", Imgcodecs.CV_LOAD_IMAGE_ANYCOLOR);
-             Mat newMat = new Mat();
-             // Size s = new Size(x, y);
-             Size s = new Size(rect2.width, rect2.height);
-            Imgproc.resize(matAdd, newMat, s);
-             Rect roi= new Rect(rect2.x,rect2.y,rect2.width,rect2.height);
-             Mat destinationROI = frame.submat( roi );
-             newMat.copyTo( destinationROI , newMat); 
-        
-       
-        // Rect rectCrop = new Rect(rect[0].x,rect[0].y, x1, y1);
-       
+           
+           
+    
         
         
 
-        //newMat.copyTo(frame.rowRange(rect[0].x+1, x1+1),frame.colRange(rect[0].y+1, y1+1));
-        Imgcodecs.imencode(".bmp", frame, mem);
+       
+        Imgcodecs.imencode(".bmp", matAdd, mem);
         Image im2 = new Image(new ByteArrayInputStream(mem.toArray()));
           //Mat destinationROI = im2.submat();
         imvCam2.setImage(im2);
         
-        Parent tableViewParent = FXMLLoader.load(getClass().getResource("SceltaSiNo.fxml"));
-        Scene tableViewScene = new Scene(tableViewParent);
-
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-        window.setScene(tableViewScene);
-        window.show();
+        
     }
 
     @Override
